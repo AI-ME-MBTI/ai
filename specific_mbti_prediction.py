ac61cb53fac96f77ef2c8846473406912434b56b
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-11-28 07:30:33 +0900

    feat: 모델 학습 알고리즘 작성

--- specific_mbti_prediction.py
+++ specific_mbti_prediction.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+from joblib import dump
+
 detail = pd.read_csv('mbti_detail_final.csv')
 all_words = detail['word'].unique()
 
@@ -11,3 +16,38 @@ def make_grouped_mbti():
     grouped.drop(['word_list'], axis=1, inplace=True)
     
     return grouped
+
+def train_model():
+    grouped = make_grouped_mbti()
+    personalities = ['I', 'E',  'S', 'N', 'T', 'F', 'J', 'P']
+    
+    for i in personalities:
+        temp = grouped.copy()
+        temp['mbti'] = temp['mbti'].apply(lambda x: 1 if x==i else 0)
+        X = temp.drop('mbti',axis=1)
+        y = temp['mbti']
+    
+        model = RandomForestClassifier(n_jobs=-1,n_estimators=100, random_state=42, class_weight='balanced')
+    
+        params = {'n_estimators':[100],
+              'max_depth':[3,5,7,10,12,15],
+              'max_features':['auto', 'log2', None],
+              'criterion':["gini","entropy"]}
+    
+        grid_search = GridSearchCV(estimator=model,param_grid=params,verbose=1,n_jobs=-1,scoring='accuracy', cv=StratifiedKFold(n_splits=3))
+        
+        grid_search.fit(X,y)
+    
+        Model_best = grid_search.best_estimator_
+    
+        dump(Model_best, 'model_detail_{0}.joblib'.format(i))
+    
+# train_model()
+    
+def user_text_to_datagrame(answer: str):
+    text = pd.DataFrame({'Text':answer})
+
+    for i in all_words:
+        text[i] = text['Text'].apply(lambda x: 1 if i in x.split(' ') else 0)
+    text.drop(['Text'],axis=1,inplace=True)
+    return text
