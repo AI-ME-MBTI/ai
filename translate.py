9c72eb97314c36b38bffb295acd86083d5bd1293
Hyosun Kim <84787653+hy5sun@users.noreply.github.com>
2023-12-09 01:15:30 +0900

    fix: deepl 로직으로 수정

---
+++ translate.py
@@ -0,0 +1,16 @@
+import deepl
+
+from starlette.config import Config
+from dotenv import load_dotenv
+
+load_dotenv()
+
+config = Config("./.env")
+AUTH_KEY = config.get('AUTH_KEY')
+
+def get_translate(text):
+    auth_key = AUTH_KEY
+    translator = deepl.Translator(auth_key)
+
+    result = translator.translate_text(text, target_lang="EN-US")
+    return result
